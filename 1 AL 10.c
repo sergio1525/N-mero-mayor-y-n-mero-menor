b8c6b287ed07169b390f45d239d5d1fc9f125c84
sergio] <rodrigo8902009@hotmail.com>
2015-10-27 17:43:11 -0600

    first commit

---
+++ 1 AL 10.c
@@ -0,0 +1,118 @@
+#include<stdio.h>
+#include<stdlib.h>
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+int g;
+int h;
+int i;
+int j;
+int main()
+{
+	printf("PRIMER NUMERO\n");
+	scanf("%d" ,&a);
+	printf("SEGUNDO NUMERO\n");
+	scanf("%d" ,&b);
+	printf("TERCER NUMERO\n");
+	scanf("%d" ,&c);
+	printf("CUARTO NUMERO\n");
+	scanf("%d" ,&d);
+	printf("QUINTO NUMERO\n");
+	scanf("%d" ,&e);
+	printf("SEXTO NUMERO\n");
+	scanf("%d" ,&f);
+	printf("SEPTIMO NUMERO\n");
+	scanf("%d" ,&g);
+	printf("OCTAVO NUMERO\n");
+	scanf("%d" ,&h);
+	printf("NOVENO NUMERO\n");
+	scanf("%d" ,&i);
+	printf("DECIMO NUMERO\n");
+	scanf("%d" ,&j);
+	if(a>=b && a>=c  && a>=d && a>=e && a>=f && a>=g  && a>=h && a>=i && a>=j )
+{
+	printf("EL MAYOR ES %d\n",a);
+}
+	else if(b>=a && b>=c && b>=d && b>=e && b>=f && b>=g && b>=h  && b>=i&& b>=j )
+{
+	printf("EL MAYOR ES %d\n",b);
+}
+	else if(c>=a && c>=b && c>=d && c>=e && c>=f && c>=g && c>=h && c>=i && c>=j )
+{
+	printf("EL MAYOR ES %d\n",c);
+}
+	else if(d>=a && d>=b && d>=c  && d>=e && d>=f && d>=g && d>=h && d>=i && d>=j)
+{
+	printf("EL MAYOR ES %d\n",d);
+}
+	else if(e>=a && e>=b && e>=c && e>=d && e>=f && e>=g && e>=h && e>=i && e>=j)
+{
+	printf("EL MAYOR ES %d\n",e);
+}
+	else if(f>=a && f>=b && f>=c && f>=d && f>=e && f>=g && f>=h && f>=i && f>=j)
+{
+	printf("EL MAYOR ES %d\n",f);
+}
+	else if(g>=a && g>=b && g>=c && g>=d && g>=e && g>=f && g>=h && g>=i && g>=j)
+{
+	printf("EL MAYOR ES %d\n",g);
+}
+	else if(h>=a && h>=b && h>=c && h>=d && h>=e && h>=f && h>=g && h>=i && h>=j)
+{
+	printf("EL MAYOR ES %d\n",h);
+}
+	else if(i>=a && i>=b && i>=c && i>=d && i>=e && i>=f && i>=g && i>=h && i>=j)
+{
+	printf("EL MAYOR ES %d\n",i);
+}
+	else if(j>=a && j>=b && j>=c && j>=d && j>=e && j>=f && j>=g && j>=h && j>=i)
+{
+	printf("EL MAYOR ES %d\n",i);
+}
+else
+{
+}
+	if(a<b && a<c  && a<d && a<e && a<f && a<g  && a<h && a<i && a<j)
+{
+	printf("EL MENOR ES %d\n",a);
+}
+	else if(b<a && b<c && b<d  && b<e && b<f && b<g && b<h  && b<i && b<j)	
+{
+	printf("EL MENOR ES %d\n",b);
+}
+	else if(c<a && c<b && c<d && c<e && c<f && c<g && c<h && c<i &&c<j)
+{
+	printf("EL MENOR ES %d\n",c);
+}
+	else if(d<a && d<b && d<c  && d<e && d<f && d<g && d<h  && d<i && d<j)
+{
+	printf("EL MENOR ES %d\n",d);
+}	
+	else if(e<a && e<b && e<c  && e<d && e<f && e<g && e<h  && e<i &&e<j)
+{
+	printf("EL MENOR ES %d\n",e);
+}
+	else if(f<a && f<b && f<c && f<d && f<e && f<g && f<h && f<i && f<j)
+{
+	printf("EL MENOR ES %d\n",f);
+}
+	else if(g<a && g<b && g<c && g<d && g<e && g<f && g<h && g<i && g<j)
+{
+	printf("EL MENOR ES %d\n",g);
+}
+	else if(h<a && h<b && h<c && h<d && h<e && h<f && h<g && h<i && h<j)
+{
+	printf("EL MENOR ES %d\n",h);
+}
+	else if(i<a && i<b && i<c && i<d && i<e && i<f && i<g && i<h && i<j)
+{
+	printf("EL MENOR ES %d\n",i);
+}
+	else if(j<a && j<b && j<c && j<d && j<e && j<f && j<g && j<h && j<i)
+{
+	printf("EL MENOR ES %d\n",j);
+}system("PAUSE");
+}
